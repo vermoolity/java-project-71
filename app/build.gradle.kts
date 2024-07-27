ac27302de0d525f34a6b175adaff9aeb5454cc9e
Светлана Попова <pixeltiger@yandex.ru>
2024-07-28 04:17:35 +0500

    start of the project

---
+++ app/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    application
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+application {
+    mainClass = "hexlet.code.App"
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
