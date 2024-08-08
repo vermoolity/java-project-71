e7af9b87c991830a52189eeeb54df7b86e2d3844
Светлана Попова <pixeltiger@yandex.ru>
2024-08-08 22:52:30 +0500

    4 task

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,7 @@ application {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.6")
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,23 @@
 package hexlet.code;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
 public class App {
+    @Parameters(paramLabel = "filepath1",
+            description = "path to first file")
+    String ref;
+    @Parameters(paramLabel = "filepath2",
+            description = "path to second file")
+    String re;
+    @Option(names = {"-f ", "--format"}, description = "output format [default: stylish]")
+    String format;
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
